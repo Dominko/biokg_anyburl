9bd1961f16295925ad05d18b29728c34b418a8ef
Dominik Grabarczyk <co-grab1@login-e-14.data.cluster>
2023-02-13 13:47:52 +0000

    Tweaked for CSD3

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,7 +2,8 @@
 
 # Recompile
 cd AnyBURL/
-javac de/unima/ki/anyburl/*.java -d build
+#javac de/unima/ki/anyburl/*.java -d build
+javac de/unima/ki/anyburl/*.java de/unima/ki/anyburl/algorithm/*.java de/unima/ki/anyburl/data/*.java de/unima/ki/anyburl/eval/*.java de/unima/ki/anyburl/exceptions/*.java de/unima/ki/anyburl/io/*.java de/unima/ki/anyburl/playground/*.java de/unima/ki/anyburl/structure/*.java de/unima/ki/anyburl/threads/*.java de/unima/ki/anyburl/structure/compare/*.java -d build
 jar cfv AnyBURL-23-1.jar -C build .
 cd ../
 
@@ -12,9 +13,9 @@ java -Xmx12G -cp AnyBURL/AnyBURL-23-1.jar de.unima.ki.anyburl.Learn config/confi
 # Make predictions
 for i in {100..1000..100}
 do
-    sed -i "5s#.*#PATH_RULES = data/rules.txt-$i#" config/config-AnyBURL-apply
-    sed -i "6s#.*#PATH_OUTPUT = data/predictions-max-$i#" config/config-AnyBURL-apply
-    sed -i "6s#.*#PATH_PREDICTIONS = data/predictions-max-$i#" config/config-AnyBURL-eval
+    sed -i "5s#.*#PATH_RULES = /rds/user/co-grab1/hpc-work/anyburl/rules-$i#" config/config-AnyBURL-apply
+    sed -i "6s#.*#PATH_OUTPUT = /rds/user/co-grab1/hpc-work/anyburl/predictions-max-$i#" config/config-AnyBURL-apply
+    sed -i "6s#.*#PATH_PREDICTIONS = /rds/user/co-grab1/hpc-work/anyburl/predictions-max-$i#" config/config-AnyBURL-eval
     
     java -Xmx12G -cp AnyBURL/AnyBURL-23-1.jar de.unima.ki.anyburl.Apply config/config-AnyBURL-apply
     java -Xmx12G -cp AnyBURL/AnyBURL-23-1.jar de.unima.ki.anyburl.Eval config/config-AnyBURL-eval
