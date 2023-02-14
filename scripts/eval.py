f0d23bf8e82e51a5acc9599f53b8a82220fe395b
Dominik Grabarczyk <s1873532@ed.ac.uk>
2023-02-14 17:09:24 +0000

    fixed evaulation script

--- scripts/eval.py
+++ scripts/eval.py
@@ -4,69 +4,141 @@ from scipy.stats import rankdata
 from tqdm import tqdm
 import sys
 
-def read_predictions(path):
-    with open(path, encoding="utf8") as infile:
-        while True:
-            triple = infile.readline().strip().split(" ")
-            if not triple or triple[0] == "":
-                break
-            head,rel,tail = triple
-            pred_heads = infile.readline().strip()[7:].split("\t")
-            pred_tails = infile.readline().strip()[7:].split("\t")
-            
-            confidences_head = [int(x.replace("0.", "").replace("1.","1").ljust(100, "0")) if (not x.startswith("1.") and not x.startswith("1")) else int("1".ljust(101, "0")) for x in pred_heads[1::2]]
-            confidences_tail = [int(x.replace("0.", "").replace("1.","1").ljust(100, "0")) if (not x.startswith("1.") and not x.startswith("1")) else int("1".ljust(101, "0")) for x in pred_tails[1::2]]
-            
-            yield (head, pred_heads[0::2], confidences_head)
-            yield (tail, pred_tails[0::2], confidences_tail)
-        
+import pandas as pd
+from io import StringIO
 
-def get_n_test(path):
-    content = None
-    with open(path, encoding="utf8") as infile:
-        content = infile.readlines()
-    content = [x.strip() for x in content]
-    return len(content)       
-    
+import pickle
 
-def evaluate_policy(path_predictions, n, policy):
-    hits1 = 0
-    hits3 = 0
-    hits10 = 0
-    hits50 = 0
-    hits100 = 0
-    mrr = 0.0
-    mr = 0
-    
-    for true_entity, prediction, conf in read_predictions(path_predictions):
-        ranking = rankdata([-x for x in conf], method=policy)
+from threading import Thread, Lock
+from queue import Queue
+
+from time import time
+
+def parse_preds(path_predictions):
+    with open(path_predictions) as prediction_file:
+        lines = prediction_file.readlines()
+
+        # Sanity check if everything is correct
+        assert len(lines) % 3 == 0
+
+        all_heads = pd.DataFrame(columns=["triple", "prediction", "confidence"])
+        all_tails = pd.DataFrame(columns=["triple", "prediction", "confidence"])
+
+        heads = []
+        tails = []
+
+        test = 0
+        for i in tqdm(range(test, len(lines)//3)):
+            triple = lines[i*3].strip()
+
+            # Extract heads (DISGUSTING)
+            heads.append(lines[i*3 + 1].split("Heads: ")[1].strip())
+            if not heads[i-test] == "":
+                heads[i-test] = StringIO(heads[i-test])
+                heads[i-test] = pd.read_csv(heads[i-test], sep="\t", header=None).to_numpy()
+                heads[i-test] = pd.DataFrame(heads[i-test].reshape((len(heads[i-test][0])//2,2)), columns=["prediction", "confidence"])
+                heads[i-test]["prediction"] = heads[i-test]["prediction"].astype(int).astype(str) + " " + triple.split(" ")[1] + " " + triple.split(" ")[2]
+                heads[i-test]["triple"] = triple
+                # heads.set_index("triple", inplace = True)
+            else:
+                heads[i-test] = pd.DataFrame(columns=["triple", "prediction", "confidence"])
+                # heads.set_index("triple", inplace = True)
+                # TODO: If missing, 0, add to log
+
+            # Extract tails (DISGUSTING)
+            tails.append(lines[i*3 + 2].split("Tails: ")[1].strip())
+            if not tails[i-test] == "":
+                tails[i-test] = StringIO(tails[i-test])
+                tails[i-test] = pd.read_csv(tails[i-test], sep="\t", header=None).to_numpy()
+                tails[i-test] = pd.DataFrame(tails[i-test].reshape((len(tails[i-test][0])//2,2)), columns=["prediction", "confidence"])
+                tails[i-test]["prediction"] = triple.split(" ")[0] + " " + triple.split(" ")[1] + " " + tails[i-test]["prediction"].astype(int).astype(str)
+                tails[i-test]["triple"] = triple
+                # tails.set_index("triple", inplace = True)
+            else:
+                tails[i-test] = pd.DataFrame(columns=["triple", "prediction", "confidence"])
+                # tails.set_index("triple", inplace = True)
+                # TODO: If missing, 0, add to log
+            
+
+            
+            # print(triple)
+            # print(heads)
+            # print(tails)
+
+            # if i == 500:
+            #     break
+        all_heads = pd.concat(heads)
+        all_tails = pd.concat(tails)
+
+        print(all_heads)
+        print(all_tails)
+        return all_heads, all_tails
+
+
+def evaluate(path_predictions, path_train, path_valid, path_test, head_path, tail_path, force_regen:bool):
+    # Read prediction file and extract predictions as dict
+    if not os.path.isfile(head_path) or not os.path.isfile(tail_path):
+        print("we are missing parsed preds, generate...")
+        heads, tails = parse_preds(path_predictions)
+        with open(head_path, 'wb') as f:
+            pickle.dump(heads, f)
+        with open(tail_path, 'wb') as f:
+            pickle.dump(tails, f)
+    elif force_regen:
+        print("Regeneration requested...")
+        heads, tails = parse_preds(path_predictions)
+        with open(head_path, 'wb') as f:
+            pickle.dump(heads, f)
+        with open(tail_path, 'wb') as f:
+            pickle.dump(tails, f)
+    else:
+        print("Reading heads and tails from file...")
+        with open(head_path, 'rb') as f:
+            heads = pickle.load(f)
+        with open(tail_path, 'rb') as f:
+            tails = pickle.load(f)
+
+    # Read raw data files
+    print("Loading dataset")
+    train = pd.read_csv(path_train, sep="\t",header=None)
+    valid = pd.read_csv(path_valid, sep="\t",header=None)
+    test = pd.read_csv(path_test, sep="\t",header=None)
+
+    print("Combining dataset")
+    all_triples = pd.concat([train, valid, test])
+    all_triples["triple"] = all_triples[0].astype(str) + " " + all_triples[1].astype(str) + " " + all_triples[2].astype(str)
+    all_triples = all_triples["triple"]
+
+    print("Filtering")
+    heads = heads.merge(all_triples, left_on="prediction", right_on="triple", indicator=True, how="outer").query('_merge=="left_only"|triple_x==prediction').drop(['_merge', 'triple_y'], axis=1)
+
+    # heads.sort_values("triple_x", inplace = True)
+    print("Grouping")
+    heads = heads.groupby(heads.triple_x)
+
+    true_pos = 0
+    missing = 0
+    # FOR LOOP OVER VALID
+    for index, triple in tqdm(valid.iterrows()):
+        # get valid item
+        triple = str(triple[0]) + " " + str(triple[1]) + " " + str(triple[2])
         try:
-            idx = prediction.index(true_entity)
-            rank = ranking[idx]
-            
-            if rank == 1.:
-                hits1 = hits1 + 1
-            if rank <= 3.:
-                hits3 = hits3 + 1
-            if rank <= 10.:
-                hits10 = hits10 + 1
-            if rank <= 50.:
-                hits50 = hits50 + 1
-            if rank <= 100.:
-                hits100 = hits100 + 1
-            mrr = mrr + (1 / rank)
-        except ValueError:
-            pass
-    #return hits1/n, hits3/n, hits10/n, mrr/n
-    return "MRR: %.3f" % (mrr/n), "Hits@1: %.3f" % (hits1/n), "Hits@3: %.3f" % (hits3/n) , "Hits@10: %.3f" % (hits10/n), "Hits@50: %.3f" % (hits50/n) , "Hits@100: %.3f" % (hits100/n) 
+            top10 = heads.get_group(triple)
+            top10 = top10.sort_values(by=["confidence"], ascending=False)
+            top10 = top10.reset_index()
+            # # Rank
+            if triple in top10["prediction"][0:10].values:
+                true_pos += 1
+        except:
+            missing += 1
 
-def evaluate(path_predictions, path_test):
-    n = get_n_test(path_test) * 2
-    #["ordinal", "average", "min", "max", "dense"]
-    result = evaluate_policy(path_predictions, n, "average")
-    return "\n".join(result)
+    print(path_predictions)
+    print("true positives = " + str(true_pos))
+    print("missing = " + str(missing))
+    print("hits at 10 = " + str(true_pos/len(valid)))
+
 
 
 if __name__ == "__main__":
-    res = evaluate(sys.argv[1], sys.argv[2])
+    res = evaluate(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7] == 'True')
     print(res)
