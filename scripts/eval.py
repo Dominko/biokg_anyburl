e5c7ebd10458c94375dc3d5ec88d53401132fc2d
Dominik Grabarczyk <s1873532@ed.ac.uk>
2023-06-06 13:20:42 +0100

    Added MRR calculation to code

--- scripts/eval.py
+++ scripts/eval.py
@@ -30,13 +30,13 @@ def parse_preds(path_predictions):
         for i in tqdm(range(test, len(lines)//3)):
             triple = lines[i*3].strip()
 
-            # Extract heads (DISGUSTING)
+            # Extract heads
             heads.append(lines[i*3 + 1].split("Heads: ")[1].strip())
             if not heads[i-test] == "":
                 heads[i-test] = StringIO(heads[i-test])
                 heads[i-test] = pd.read_csv(heads[i-test], sep="\t", header=None).to_numpy()
                 heads[i-test] = pd.DataFrame(heads[i-test].reshape((len(heads[i-test][0])//2,2)), columns=["prediction", "confidence"])
-                heads[i-test]["prediction"] = heads[i-test]["prediction"].astype(str) + " " + triple.split(" ")[1] + " " + triple.split(" ")[2]
+                heads[i-test]["prediction"] = heads[i-test]["prediction"].astype(int).astype(str) + " " + triple.split(" ")[1] + " " + triple.split(" ")[2]
                 heads[i-test]["triple"] = triple
                 # heads.set_index("triple", inplace = True)
             else:
@@ -44,13 +44,13 @@ def parse_preds(path_predictions):
                 # heads.set_index("triple", inplace = True)
                 # TODO: If missing, 0, add to log
 
-            # Extract tails (DISGUSTING)
+            # Extract tails
             tails.append(lines[i*3 + 2].split("Tails: ")[1].strip())
             if not tails[i-test] == "":
                 tails[i-test] = StringIO(tails[i-test])
                 tails[i-test] = pd.read_csv(tails[i-test], sep="\t", header=None).to_numpy()
                 tails[i-test] = pd.DataFrame(tails[i-test].reshape((len(tails[i-test][0])//2,2)), columns=["prediction", "confidence"])
-                tails[i-test]["prediction"] = triple.split(" ")[0] + " " + triple.split(" ")[1] + " " + tails[i-test]["prediction"].astype(str)
+                tails[i-test]["prediction"] = triple.split(" ")[0] + " " + triple.split(" ")[1] + " " + tails[i-test]["prediction"].astype(int).astype(str)
                 tails[i-test]["triple"] = triple
                 # tails.set_index("triple", inplace = True)
             else:
@@ -104,7 +104,7 @@ def evaluate(path_predictions, path_train, path_valid, path_test, head_path, tai
     test = pd.read_csv(path_test, sep="\t",header=None)
 
     print("Combining dataset")
-    all_triples = pd.concat([train, valid, test])
+    all_triples = pd.concat([train, test])
     all_triples["triple"] = all_triples[0].astype(str) + " " + all_triples[1].astype(str) + " " + all_triples[2].astype(str)
     all_triples = all_triples["triple"]
 
@@ -112,25 +112,37 @@ def evaluate(path_predictions, path_train, path_valid, path_test, head_path, tai
     heads = heads.merge(all_triples, left_on="prediction", right_on="triple", indicator=True, how="outer").query('_merge=="left_only"|triple_x==prediction').drop(['_merge', 'triple_y'], axis=1)
     tails = tails.merge(all_triples, left_on="prediction", right_on="triple", indicator=True, how="outer").query('_merge=="left_only"|triple_x==prediction').drop(['_merge', 'triple_y'], axis=1)
 
+    print(heads)
 
     # heads.sort_values("triple_x", inplace = True)
     print("Grouping")
     heads = heads.groupby(heads.triple_x)
     tails = tails.groupby(tails.triple_x)
 
+    
+
     true_pos = 0
     missing = 0
+    rr = 0
     # FOR LOOP OVER VALID
-    for index, triple in tqdm(valid.iterrows()):
+    for index, triple in tqdm(test.iterrows()):
         # get valid item
         triple = str(triple[0]) + " " + str(triple[1]) + " " + str(triple[2])
+        top10 = ""
         try:
             top10 = heads.get_group(triple)
             top10 = top10.sort_values(by=["confidence"], ascending=False)
             top10 = top10.reset_index()
+
+            try:
+                triple_idx = int(top10.loc[top10["prediction"] == triple].index[0])
+                rr += 1.0/triple_idx
+            except:
+                rr += 0
             # # Rank
             if triple in top10["prediction"][0:10].values:
                 true_pos += 1
+            # if triple_idx 
         except:
             missing += 1
 
@@ -138,6 +150,12 @@ def evaluate(path_predictions, path_train, path_valid, path_test, head_path, tai
             top10 = tails.get_group(triple)
             top10 = top10.sort_values(by=["confidence"], ascending=False)
             top10 = top10.reset_index()
+
+            try:
+                triple_idx = int(top10.loc[top10["prediction"] == triple].index[0])
+                rr += 1.0/triple_idx
+            except:
+                rr += 0
             # # Rank
             if triple in top10["prediction"][0:10].values:
                 true_pos += 1
@@ -147,7 +165,8 @@ def evaluate(path_predictions, path_train, path_valid, path_test, head_path, tai
     print(path_predictions)
     print("true positives = " + str(true_pos))
     print("missing = " + str(missing))
-    print("hits at 10 = " + str(true_pos/len(valid)))
+    print("hits at 10 = " + str(true_pos/(len(test)*2)))
+    print("MRR = " + str(rr/(len(test)*2)))
 
     results = []
     try:
